759ee843eb4c1a9b92e304c8e95c9b585f7cd4f5
舘本隆慈 <tatemotoryuji@Fine-Mac-Mini.local>
2026-05-06 18:33:56 +0900

    redesign: ゆからしいダークテーマに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yuka-picks.jp',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
